202040af1565f16fd2d87ff817a04f1892153b13
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-24 19:51:25 +0100

    cli for governify ecosystem

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
 # governify-cli
+
+## 1. Install governify-cli
+```bash
+npm install governify-cli -g
+```
+## 2. Use cli
+```bash
+isa@host:~/lab$ gov
+governify-cli$
+
+ ```
+
+ A prompt will be displayed as "governify-cli"
+
+### 2.1. Help inside own cli
+
+```bash
+governify-cli$ help
+
+  Commands:
+
+    help [command...]                                       Provides help
+                                                            for a given
+                                                            command.
+    exit                                                    Exits
+                                                            application.
+    testlimittime <url> <method> <count> <duration> [body]  
+    testinfinite <url> <method> <count> [body]              
+    testfromfile <path>                                     
+    ls [dir]                                                
+    mkdir [dir]                                             
+    pwd [dir]
+```
+### 2.2 Examples
+
+Where are cli executing...
+
+```bash
+governify-cli$ pwd
+/home/quique/lab
+```
+Doing any tests
+```bash
+governify-cli$ testlimittime http://www.google.es GET 3 2
+{ totalRequest: 6,
+  success: 6,
+  error: 0,
+  totalStack: 2,
+  startTime: "2017-03-24T18:34:01.229Z",
+  finishTime: "2017-03-24T18:34:03.477Z",
+  lastedFor: "2248 ms",
+  url: "http://www.google.es",
+  method: 'GET' }
+
+```
+
+## 3. Use cli from bash shell
+
+You can interact with the command called gov
+
+
+### 3.1. Help outside cli
+
+```bash
+
+isa@host:~/lab$ gov --help
+
+  Usage: gov [options] [command]
+
+
+  Commands:
+
+    test [options]   read test from file
+    cli              enter cli mode
+
+
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
+
+```
+
+### 3.2. Examples
+
+```bash
+gov test --infiniteRequests -u http://google.es -c 5  -m GET
+{ totalRequest: 5,
+  success: 5,
+  error: 0,
+  startTime: '2017-03-24T18:41:05.759Z',
+  finishTime: '2017-03-24T18:41:06.292Z',
+  lastedFor: '533 ms' }
+```
+
+
+
+## Latest release
+
+The version 0.0.0 is the latest stable version of project-template-nodejs component.
+see [release note](https://github.com/isa-group/governify-cli/releases/tag/0.0.0) for details.
+
+For running:
+
+- Download latest version from [0.0.0](https://github.com/isa-group/governify-cli/releases/tag/0.0.0)
---
+++ src/index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+/*!
+
+project-template-nodejs 0.0.0, built on: 2017-03-24
+Copyright (C) 2017 ISA group
+http://www.isa.us.es/
+https://github.com/isa-group/governify-cli
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+"use strict";
+const program = require('commander');
+const test = require('./lib/test');
+const cli = require('./lib/cli');
+
+
+
+
+
+program.version("0.0.1");
+if (process.argv.length==2){
+ cli();
+}
+
+
+program
+    .command('test')
+    .description('read test from file')
+    .on('--help', function() {
+        console.log('  Examples:');
+        console.log('');
+        console.log('   - Read from file');
+        console.log('    $ gov test --readFromFile /home/isa/lab/parallel-requests/src/configurations/config-exec.yaml');
+        console.log('');
+        console.log('   - Limit tests');
+        console.log('    $ gov test --timeResquests -u http://google.es -c 5 -d 2 -m GET');
+        console.log('');
+        console.log('   - Infinite tests');
+        console.log('    $ gov test --infiniteRequests -u http://google.es -c 5  -m GET');
+        console.log('');
+    })
+    .option('-r, --readFromFile', 'Option for reading file of tests')
+    .option('-t, --timeResquests', 'Option for testing with limit tests')
+    .option('-i, --infiniteRequests', 'Option for testing infinite times')
+    .option('-p, --path <directory>', 'put the filepath')
+    .option('-c, --count <count>', 'put the count')
+    .option('-u, --url <url>', 'put the url')
+    .option('-d, --duration <duration>', 'put the duration')
+    .option('-m, --method <method>', 'put the method')
+    .option('-b, --body <body>', 'put the body')
+    .action(test);
+
+program
+    .command('cli')
+    .description('enter cli mode')
+    .action(cli);
+
+
+program
+      .command('')
+      .action(function(){
+         program.outputHelp();
+      });
+
+program.parse(process.argv);
+
+module.exports = program;
+
+//node command.js readFile /home/quique/lab/parallel-requests/src/configurations/config-exec.yaml
+
+//node command.js timeResquests -u http://google.es -c 5 -d 2 -m GET
+//node command.js infiniteRequests -u http://google.es -c 5  -m GET
