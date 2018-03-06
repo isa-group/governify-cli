e1108af0ddc4d77dd59e8ecc5db6ca7207b4e955
Mauraza <Ibone.Rosa@gmail.com>
2018-03-06 15:56:53 +0100

    little change about url

--- src/index.js
+++ src/index.js
@@ -20,61 +20,51 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
-
 "use strict";
-const program = require('commander');
-const test = require('./lib/test');
-const cli = require('./lib/cli');
-
-
-
-
+const program = require("commander");
+const test = require("./lib/test");
+const cli = require("./lib/cli");
 
 program.version("0.0.4");
-if (process.argv.length==2){
- cli();
+if (process.argv.length == 2) {
+  cli();
 }
 
+program
+  .command("test")
+  .description("read test from file")
+  .on("--help", function() {
+    console.log("  Examples:");
+    console.log("");
+    console.log("   - Read from file");
+    console.log("    $ gov test --readFromFile /home/isa/lab/parallel-requests/src/configurations/config-exec.yaml");
+    console.log("");
+    console.log("   - Limit tests");
+    console.log("    $ gov test --timeResquests -u http://google.es -c 5 -d 2 -m GET");
+    console.log("");
+    console.log("   - Infinite tests");
+    console.log("    $ gov test --infiniteRequests -u http://google.es -c 5  -m GET");
+    console.log("");
+  })
+  .option("-r, --readFromFile", "Option for reading file of tests")
+  .option("-t, --timeResquests", "Option for testing with limit tests")
+  .option("-i, --infiniteRequests", "Option for testing infinite times")
+  .option("-p, --path <directory>", "put the filepath")
+  .option("-c, --count <count>", "put the count")
+  .option("-u, --url <url>", "put the url")
+  .option("-d, --duration <duration>", "put the duration")
+  .option("-m, --method <method>", "put the method")
+  .option("-b, --body <body>", "put the body")
+  .action(test);
 
 program
-    .command('test')
-    .description('read test from file')
-    .on('--help', function() {
-        console.log('  Examples:');
-        console.log('');
-        console.log('   - Read from file');
-        console.log('    $ gov test --readFromFile /home/isa/lab/parallel-requests/src/configurations/config-exec.yaml');
-        console.log('');
-        console.log('   - Limit tests');
-        console.log('    $ gov test --timeResquests -u http://google.es -c 5 -d 2 -m GET');
-        console.log('');
-        console.log('   - Infinite tests');
-        console.log('    $ gov test --infiniteRequests -u http://google.es -c 5  -m GET');
-        console.log('');
-    })
-    .option('-r, --readFromFile', 'Option for reading file of tests')
-    .option('-t, --timeResquests', 'Option for testing with limit tests')
-    .option('-i, --infiniteRequests', 'Option for testing infinite times')
-    .option('-p, --path <directory>', 'put the filepath')
-    .option('-c, --count <count>', 'put the count')
-    .option('-u, --url <url>', 'put the url')
-    .option('-d, --duration <duration>', 'put the duration')
-    .option('-m, --method <method>', 'put the method')
-    .option('-b, --body <body>', 'put the body')
-    .action(test);
+  .command("cli")
+  .description("enter cli mode")
+  .action(cli);
 
-program
-    .command('cli')
-    .description('enter cli mode')
-    .action(cli);
-
-
-program
-      .command('')
-      .action(function(){
-         program.outputHelp();
-      });
+program.command("").action(function() {
+  program.outputHelp();
+});
 
 program.parse(process.argv);
 
